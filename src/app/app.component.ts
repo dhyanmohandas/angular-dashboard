a8719398ca77735b540e7e917513310f39ff811b
Dhyan Mohandas <dhyanmohandas@gmail.com>
2021-03-28 15:57:18 +0530

    Checkbox to radio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,40 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular Dashboard';
   checkValue: Boolean = true;
+  RiskRoles = [
+    {
+      Id:1,
+      Name:'Low',
+      Checked:true
+    },
+    {
+      Id:2,
+      Name:'Medium',
+      Checked:false
+    },
+    {
+      Id:3,
+      Name:'High',
+      Checked:false
+    }
+  ];
+
+  radioSel:any;
+  radioSelected:string;
+  radioSelectedString:string;
+
+  getSelecteditem(){
+    this.radioSel = this.RiskRoles.find(Item => Item.Name === this.radioSelected);
+    this.radioSelectedString = JSON.stringify(this.radioSel);
+  }
+
+  constructor() {
+    this.radioSelected = "Low";
+    this.getSelecteditem();
+   }
+
+  onItemChange(item){
+    this.getSelecteditem();
+  }
+ 
 }
